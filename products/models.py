a4f6a94e0442274dd70afeb6fc65d909eeafdada
TusharCitrusbug <98886632+TusharCitrusbug@users.noreply.github.com>
2022-04-25 18:50:59 +0530

    added models

--- products/models.py
+++ products/models.py
@@ -1,9 +1,16 @@
+from pickle import FALSE
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
-    name=models.CharField(max_length=100)
-    price= models.IntegerField(1000) #cents
-
+    Name=models.CharField(unique=True,max_length=100)
+    Description=models.TextField()
+    Price= models.FloatField(max_length=10000,null=False,blank=False) #cents
+    Image=models.FileField(upload_to="images/", blank=True, null=True)
     def __str__(self):
-        return self.name
+        return self.Name
+
+class Customer(models.Model):
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.email
--- strip_api/settings.py
+++ strip_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from decouple import config
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,13 +76,23 @@ WSGI_APPLICATION = 'strip_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stripe_db', 
+        'USER': 'postgres', 
+        'PASSWORD': 'root',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -126,3 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STRIP_PRIVATE_KEY=config('STRIP_PRIVATE_KEY')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
